8c65773dded238439291edbdced4d91f2fa7198a
Greg Stewart <contact@gjstewart.net>
2019-06-03 05:40:40 -0400

    Added comments for how the programs will work.

--- SwitchController/SwitchController.ino
+++ SwitchController/SwitchController.ino
@@ -1,3 +1,33 @@
+/*
+ * This code is what sends the signal to flip the switch.
+ * 
+ * Components:
+ *   - power LED
+ *   - status LED
+ *   - toggle switch
+ *   - "send off button"?
+ * 
+ * When run:
+ *   - Sets relay to OFF
+ *   - listens for signal that switch activated and local switch flipped
+ *   - turns on power LED
+ *   - When switch flipped On:
+ *     - sends signal to reciever to activate the switch
+ *     - lights up status LED half/ yellow
+ *   - When recieved ON confirmation:
+ *     - lights up status LED full/ red
+ *   - When switch flipped off or off button hit:
+ *     - sends signal to reciever to turn off switch
+ *     - lights up status LED half/yellow
+ *   - When recieved OFF confirmation:
+ *     - turns off status LED
+ *     
+ * By Greg Stewart
+ * Created 6/3/19
+ */
+
+#include <SoftwareSerial.h>
+
 void setup() {
   // put your setup code here, to run once:
 
--- SwitchReciever/SwitchReciever.ino
+++ SwitchReciever/SwitchReciever.ino
@@ -1,5 +1,52 @@
+/*
+ * This code is what runs the actual relay based on what it recieves from the wireless transponder.
+ * 
+ * Components:
+ *   - Relay switch
+ *   - power LED
+ *   - status LED
+ * 
+ * When run:
+ *   - Sets relay to OFF
+ *   - listens for signal to switch on
+ *   - turns on power LED
+ *   - When recieves signal to switch:
+ *     - Switches relay on
+ *     - Sends signal out to confirm
+ *     - Turns on status LED
+ *   - When recieves signal to switch off:
+ *     - Switches relay off
+ *     - sends out signal to confirm
+ *     - Turns off status LED
+ * 
+ * 
+ * 
+ * 
+ * By Greg Stewart
+ * Created 6/3/19
+ */
+
+#include <SoftwareSerial.h>
+
+#define txPin 10
+#define rxPin 11
+
+#define setPin 6
+#define button1 4
+#define button2 3
+
+/* 
+ * setup for transiever, read/writing
+ */
+SoftwareSerial HC12(txPin, rxPin);
+byte incomingByte;
+String readBuffer = "";
+
+
 void setup() {
-  // put your setup code here, to run once:
+  //setup serial ports
+  Serial.begin(9600);
+  HC12.begin(9600);
 
 }
 
