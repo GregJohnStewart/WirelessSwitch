c41c03e98f45085024b86773ef7594ad3b9d68b5
Greg Stewart <contact@gjstewart.net>
2019-06-11 21:59:34 -0400

    Initial files for the switch controller

--- SwitchController/SwitchController.ino
+++ SwitchController/SwitchController.ino
@@ -28,6 +28,35 @@
 
 #include <SoftwareSerial.h>
 
+#define baudRate 9600
+
+// for communicating with HC-12
+#define txPin 10
+#define rxPin 11
+#define setPin 12
+
+// For other functionality
+#define readyLed 8 // switched ON when setup is complete and ready to recieve
+#define statusLedR 7 // LED to indicate status of switch
+#define statusLedG 6 // LED to indicate status of switch
+#define statusLedB 5 // LED to indicate status of switch
+
+/*
+ * Constants for signals
+ */
+#define onBytes String("SET RELAY ON")
+#define offBytes String("RELAY OFF")
+
+/*
+ * Constants for buttons
+ */
+#define BUTTON1_PIN               2  // Button 1
+#define BUTTON2_PIN               3  // Button 2
+
+#define DEFAULT_LONGPRESS_LEN    25  // Min nr of loops for a long press
+#define DELAY                    20  // Delay per loop in ms
+
+
 void setup() {
   // put your setup code here, to run once:
 
---
+++ SwitchController/Transciever.ino
@@ -0,0 +1,50 @@
+/*
+   This code controls the HC-12 transciever
+*/
+SoftwareSerial HC12(txPin, rxPin);
+byte incomingByte;
+String readBuffer = "";
+unsigned long lastSendOnSignal = millis();
+Status curStatus = 
+
+enum Status { WAITING_OFF,  };
+
+void setupTransciever() {
+  HC12.begin(baudRate);
+  pinMode(setPin, OUTPUT);
+  digitalWrite(setPin, HIGH);  // HC-12 normal, transparent mode
+}
+
+void checkForSignal() {
+  while (HC12.available()) {             // If HC-12 has data
+    incomingByte = HC12.read();          // Store each icoming byte from HC-12
+    readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable
+  }
+
+  if (onBytes.equals(readBuffer)) {
+    signalOn = true;
+  } else if (offBytes.equals(readBuffer)) {
+    signalOn = false;
+  }
+  delay(100);
+}
+
+void sendOnConfirm() {
+  unsigned long current = millis();
+  unsigned long difference = current - lastSendOnSignal;
+
+  if (difference <= sendOnDelay) {
+    lastSendOnSignal = current;
+    if (signalOn) {
+      HC12.write(onBytes.c_str());
+    } else {
+      HC12.write(offBytes.c_str());
+    }
+    delay(100);
+  }
+}
+
+void doTransciever() {
+  checkForSignal();
+  sendOnConfirm();
+}
